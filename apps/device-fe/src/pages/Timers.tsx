7abeb1e250611fbe9901ec60affb1d3034f84ace
Brett Zelk <casey.zelk@gmail.com>
2025-12-04 16:38:13 -0600

    Add shadcn form

--- apps/device-fe/src/pages/Timers.tsx
+++ apps/device-fe/src/pages/Timers.tsx
@@ -1,11 +1,14 @@
 import { useGetTimersQuery } from "@state/api/data";
+import { Timer } from "../components/Timer.tsx";
 
 const TimersPage = () => {
 	const { data } = useGetTimersQuery();
 
 	const timersList = () => {
 		return data?.timers?.map((timer) => (
-			<div key={timer.id}>{timer.relay}</div>
+			<div key={timer.id} className="w-100">
+				<Timer timer />
+			</div>
 		));
 	};
 	return (
