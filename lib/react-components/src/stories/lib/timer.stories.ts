c72ccde44d3bc65e67d46ee2fc5e2e2c5485e9e6
Brett Zelk <casey.zelk@gmail.com>
2026-01-22 22:48:31 -0600

    Update timer component story.

--- lib/react-components/src/stories/lib/timer.stories.ts
+++ lib/react-components/src/stories/lib/timer.stories.ts
@@ -1,34 +1,42 @@
 import type { Meta, StoryObj } from '@storybook/react-vite';
-import { Timer  } from '../../lib/timer';
 import { expect } from 'storybook/test';
+import { Timer } from '../../lib/timer';
 
 const meta = {
-  component: Timer,
-  title: 'Timer',
+	component: Timer,
+	title: 'Timer',
 } satisfies Meta<typeof Timer>;
 export default meta;
 
 type Story = StoryObj<typeof Timer>;
 
 export const Primary = {
-  args: {
-    timer: {
-      id: '12345',
-      name: 'TEST TIMER 1',
-      startTime: new Date().toString(),
-      endTime: new Date().toString(),
-      enabled: false,
-    },
-    enableChange: () => {},
-    onUpdate: () => {},
-  },
+	args: {
+		timer: {
+			id: '12345',
+			name: 'TEST TIMER 1',
+			startTime: new Date('2026-01-22T14:30:00.000Z').toString(),
+			endTime: new Date('2026-01-22T23:30:00.000Z').toString(),
+			enabled: false,
+		},
+		enableChange: () => {},
+		onUpdate: () => {},
+	},
 } satisfies Story;
 
 export const Heading = {
-  args: {
-  },
-  play: async ({ canvas }) => {
-    await expect(canvas.getByText(/ReactComponents/gi)).toBeTruthy();
-  },
+	args: {
+		timer: {
+			id: '12345',
+			name: 'TEST TIMER 1',
+			startTime: new Date().toString(),
+			endTime: new Date().toString(),
+			enabled: false,
+		},
+		enableChange: () => {},
+		onUpdate: () => {},
+	},
+	play: async ({ canvas }) => {
+		await expect(canvas.getByTestId('title')).toContainHTML('TEST TIMER 1');
+	},
 } satisfies Story;
-
