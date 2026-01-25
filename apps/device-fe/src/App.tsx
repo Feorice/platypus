f08a4d4d801764abb0b4e899555225f80d37ee15
Brett Zelk <casey.zelk@gmail.com>
2026-01-24 21:17:31 -0600

    Remove atmosphere component from fe.

--- apps/device-fe/src/App.tsx
+++ apps/device-fe/src/App.tsx
@@ -1,22 +1,15 @@
 import logo from "@assets/images/logo.svg";
-import { useDHT22SensorEventsQuery } from "@state/api/socketApi";
 import { decrement, increment } from "@state/features/counter/counterSlice";
 import type { RootState } from "@state/hooks";
 import { useAppDispatch, useAppSelector } from "@state/hooks";
-import Atmosphere from "@/components/Atmosphere";
 
 function App() {
 	const count = useAppSelector((state: RootState) => state.counter.value);
 	const dispatch = useAppDispatch();
-	const { data: sensor } = useDHT22SensorEventsQuery();
 
 	return (
 		<div className="text-center">
 			<header className="min-h-screen flex flex-col items-center justify-center bg-[#282c34] text-white text-[calc(10px+2vmin)]">
-				<div>Data from App component: {sensor?.temperature}</div>
-				<div>
-					Data from Atmosphere component: <Atmosphere />
-				</div>
 				<img
 					src={logo}
 					className="h-[40vmin] pointer-events-none animate-[spin_20s_linear_infinite]"
--- apps/device-fe/src/pages/Test.tsx
+++ apps/device-fe/src/pages/Test.tsx
@@ -1,16 +1,5 @@
-import {
-	type DHT22Data,
-	useDHT22SensorEventsQuery,
-} from "@state/api/socketApi.ts";
-import AtmosphereStateless from "../components/AtmosphereStateless.tsx";
-
 const Test = () => {
-	const sensor = useDHT22SensorEventsQuery<DHT22Data>();
-	return (
-		<div>
-			<AtmosphereStateless temperature={sensor?.temperature} />
-		</div>
-	);
+	return <div>Test Page</div>;
 };
 
 export default Test;
